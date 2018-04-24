2adf74395c5745b45a514d3905501cccd8f89cd1
“marcinp55 <marcinp55@gmail.com>
2018-04-24 21:27:05 +0200

    Try-catch-finally test

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExpectionModuleRunner.java
@@ -0,0 +1,10 @@
+package com.kodilla.exception.main;
+
+import com.kodilla.exception.io.FileReader;
+
+public class ExpectionModuleRunner {
+    public static void main(String args[]) {
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+    }
+}
