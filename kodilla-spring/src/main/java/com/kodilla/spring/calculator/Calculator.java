9a2409d69465ba8252e854e914d983a4e637a57c
“marcinp55 <marcinp55@gmail.com>
2018-05-09 18:02:00 +0200

    Spring calculator2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,13 +1,19 @@
 package com.kodilla.spring.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Calculator {
+    @Autowired
     private Display display;
 
     public Calculator(Display display) {
         this.display = display;
+    }
+
+    public Calculator() {
+
     }
 
     public double add(double a, double b) {
